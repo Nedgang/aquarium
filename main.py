6cb5211f726404b6b7af2b5454f398bee0fdb3f7
Nedgang <babarouman@hotmail.fr>
2015-05-25 16:13:05 +0200

    init and quit function ready to use

--- main.py
+++ main.py
@@ -12,3 +12,33 @@ Usage:
 ##########
 import curses
 
+########
+# MAIN #
+########
+def _main_():
+    curses.beep()
+    screen.addstr("Test")
+    screen.refresh()
+
+    _quit(screen)
+
+#############
+# FUNCTIONS #
+#############
+def init():
+    screen = curses.initscr()  # New screen initialization
+    curses.start_color()       # Allow to use colors
+    curses.noecho()            # No display of keyboard
+    curses.cbreak()            # Will react directly to the key
+    screen.keypad(True)
+
+def _quit(screen):
+    curses.nocbreak()
+    screen.keypad(False)
+    curses.echo()
+    curses.endwin()
+
+##########
+# LAUNCH #
+##########
+_main_()
